cefee595d5b717e03d15bad5bed3ddcd4d5cd686
takumi-nishimura <clp13218@nitech.jp>
2024-12-19 22:37:55 +0900

    アプリケーションのバージョンを0.1.3に更新し、setup.pyにURLを追加

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="pgliveapp",
-    version="0.1.2",
+    version="0.1.3",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="takumi-nishimura",
     author_email="clp13218@nitech.jp",
+    url="https://github.com/takumi-nishimura/pgliveApp",
     python_requires=">=3.9",
     packages=find_packages(),
     install_requires=["pglive>=0.8.0", "pyqt6>=6.8.0", "uv>=1.0.0"],
